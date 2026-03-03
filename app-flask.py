b8738b1d61af3ad5ba31dc11bf7adb743a9d0f03
Mopper <moppper@proton.me>
2026-03-03 03:11:19 -0500

    feat: improve response with Python version

--- app-flask.py
+++ app-flask.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import os
+import sys
 
 app = Flask(__name__)
 
@@ -7,7 +8,8 @@ app = Flask(__name__)
 def hello():
     return jsonify({
         "message": "THIS PULL REQUEST",
-        "pod": os.environ.get("MY_POD", "unknown")
+        "pod": os.environ.get("MY_POD", "unknown"),
+	"python_version": sys.version
     })
 
 if __name__ == "__main__":
